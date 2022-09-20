e87c6f9c019087081fd1430efa4c87cbcc65f724
Nikita-Minov <nikitachryackhov555@gmail.com>
2022-09-20 23:59:26 +0300

    Добавьте файлы проекта.

---
+++ Lab03-dz/main.c
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+void main(void)
+{
+    int lightYears;
+    double astronomicalUnits;
+    setlocale(LC_ALL, "RUS");
+    printf("Введите количество световых лет(целое число):\n");
+    scanf("%d", &lightYears);
+    astronomicalUnits = lightYears * 63241.1;
+    printf("%d световых лет(года) = %f астрономических единиц", lightYears, astronomicalUnits);
+}
